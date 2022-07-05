ad33c5c4fa3e2214e52250046e352e959bc160d8
William Wall <williamjwall@gmail.com>
2022-07-05 11:04:58 -0500

    final model, app.py, changes made to scrapper and ect

--- app.py
+++ app.py
@@ -1,42 +1,32 @@
-
 import streamlit as st
+from Movie_Recommender_Model import User_input_scrapper
 
-import numpy as np
-import pandas as pd
-
-#from Movie_Recommender_Model.webscrapping import get_data
-from Movie_Recommender_Model.webscrapping import get_data
-
-# def output(movie_title):
-#     data = get_data()
-#     movie_data = data.loc[data['title'] == movie_title]
-#     text = movie_data['text']
-#     duration = movie_data['duration']
-#     genre = movie_data['genre']
-#     title = movie_data['title']
-#     return text, duration, genre, title
+from Movie_Recommender_Model.User_input_scrapper import User_Input
+from Movie_Recommender_Model.Module import MovieAssigner
 
 st.markdown("""# Movie Recommender App
 ## Input the name of a movie you like, and the app will recomend you 3 other movies based on your input.
 We use natural language processing to recommend you the movies""")
 
+text = st.text_input('Movie title', 'Ex: Life of Brian')
+st.write('The current movie title is', text)
 
-title = st.text_input('Movie title', 'Ex: Life of Brian')
-st.write('The current movie title is', title)
+st.markdown("""## You can also write your own story! We'll output 3 movies based off the story you've written!""")
+text = st.text_input('story')
 
 
-
+def User(text):
+    return User_Input.User_data(text)
 
 col1, col2, col3 = st.columns(3)
 
-with col1:
-    st.header(get_data('The Godfather')[3])
+df = MovieAssigner.calling_func(User)
 
+with col1:
+    st.header(df[0])
 
 with col2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
+    st.header(df[1])
 
 with col3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
+    st.header(df[2])
